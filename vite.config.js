c2958ab811a8ce84e4a6599e04a8d273fb062761
CNN <getchiusholshue122@gmail.com>
2026-04-11 19:09:21 +0800

    fix: 修复依赖与Vite版本

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-
-  // 👇 关键：禁用 Rolldown，换回稳定的 Esbuild
   build: {
-    outDir: 'dist',
-    rollupOptions: {},
-  },
-  experimental: {
-    // 关闭不稳定的新打包器
-    buildWithRolldown: false
+    outDir: 'dist'
   }
 })
